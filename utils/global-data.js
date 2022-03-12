c0deb34a21c8031eaca776f51bbdf04766028ebe
xuyongqin <1021151991@qq.com>
2022-03-12 11:08:22 +0800

    fix

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : '书法练习轨迹';
+    : '无为徐生';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '书法练习轨迹;
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Copyright © 2022 无为徐生 书法练习轨迹.';
