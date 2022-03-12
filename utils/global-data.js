2b80865ba578ba3a421897b46525dda57e00d0b2
xuyongqin <1021151991@qq.com>
2022-03-12 11:10:53 +0800

    1

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : '无为徐生';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : '书法练习轨迹;
+    : '书法练习轨迹';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Copyright © 2022 无为徐生 书法练习轨迹.';
